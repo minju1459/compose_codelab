696e2abf32f692d34fda946d518bc19c16a85d82
pmj1459 <76741702+minju1459@users.noreply.github.com>
2024-01-02 22:18:57 +0900

    UI 조정

--- app/src/main/java/com/example/compose_codelab/MainActivity.kt
+++ app/src/main/java/com/example/compose_codelab/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose_codelab.ui.theme.Compose_codelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,11 +32,10 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
+private fun Greeting(name: String) {
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
 }
 
 @Preview(showBackground = true)
